e5c16075cf8d70f81831d4b955bbcd3614bf2e95
salsabiilashifa11 <salsabiilashifa11@gmail.com>
2023-07-02 20:15:53 +0700

    update test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
-          <span style={{ color: 'white' }}>Shifa Salsabiila - 2023</span>
+          <span style={{ color: 'white' }}>Shifa Salsabiila S.T. - 2023</span>
         </div>
       </header>
     </div>
