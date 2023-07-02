fb04db4005444f6754174d6e528f6633fd224a38
salsabiilashifa11 <salsabiilashifa11@gmail.com>
2023-07-02 20:05:27 +0700

    added source and workflow configs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div style={{ fontSize: 30 }}>
+          <span style={{ color: 'white' }}>Shifa Salsabiila - 2023</span>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
